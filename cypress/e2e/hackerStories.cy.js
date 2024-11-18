52cb0237600b890dee10068eb7a65bd9d294d033
Rubeus Tecnologia e Inovação <ricbelinato@gmail.com>
2024-11-18 11:05:34 -0300

    feat: test checking the existence of no stories when none is returned

--- cypress/e2e/hackerStories.cy.js
+++ cypress/e2e/hackerStories.cy.js
@@ -11,7 +11,7 @@ describe('Hacker Stories', () => {
           query: initialTerm,
           page: '0'
         }
-      }).as('getStories')
+      }).as('getStories');
 
       cy.intercept({
         method: 'GET',
@@ -20,13 +20,13 @@ describe('Hacker Stories', () => {
           query: `${newTerm}`,
           page: '0'
         }
-      }).as('getNewTermStories')
+      }).as('getNewTermStories');
 
-      cy.visit('/')
-      cy.wait('@getStories')
+      cy.visit('/');
+      cy.wait('@getStories');
     })
 
-    it('shows 20 stories, then the next 20 after clicking "More"', () => {
+    it('20 stories, then the next 20 after clicking "More"', () => {
       cy.intercept({
         method: 'GET',
         pathname: '**/search',
@@ -34,33 +34,33 @@ describe('Hacker Stories', () => {
           query: initialTerm,
           page: '1'
         }
-      }).as('getMoreStories')
+      }).as('getMoreStories');
 
-      cy.get('.item').should('have.length', 20)
+      cy.get('.item').should('have.length', 20);
 
-      cy.contains('More').click()
-      cy.wait('@getMoreStories')
+      cy.contains('More').click();
+      cy.wait('@getMoreStories');
 
-      cy.get('.item').should('have.length', 40)
+      cy.get('.item').should('have.length', 40);
     })
 
     it('searches via the last searched term', () => {
-      cy.get('#search').clear().type(`${newTerm}{enter}`)
+      cy.get('#search').clear().type(`${newTerm}{enter}`);
 
-      cy.wait('@getNewTermStories')
+      cy.wait('@getNewTermStories');
 
-      cy.get(`button:contains(${initialTerm})`).should('be.visible').click()
+      cy.get(`button:contains(${initialTerm})`).should('be.visible').click();
 
-      cy.wait('@getStories')
+      cy.wait('@getStories');
 
-      cy.get('.item').should('have.length', 20)
+      cy.get('.item').should('have.length', 20);
       cy.get('.item')
         .first()
-        .should('contain', initialTerm)
+        .should('contain', initialTerm);
       cy.get(`button:contains(${newTerm})`)
-        .should('be.visible')
+        .should('be.visible');
     })
-  })
+  });
 
   context('Mocking the API', () => {
     beforeEach(() => {
@@ -68,59 +68,59 @@ describe('Hacker Stories', () => {
         'GET',
         `**/search?query=${initialTerm}&page=0`,
         { fixture: 'stories' }
-      ).as('getStories')
+      ).as('getStories');
 
-      cy.visit('/')
-      cy.wait('@getStories')
+      cy.visit('/');
+      cy.wait('@getStories');
     })
 
     it('shows the footer', () => {
       cy.get('footer')
         .should('be.visible')
-        .and('contain', 'Icons made by Freepik from www.flaticon.com')
+        .and('contain', 'Icons made by Freepik from www.flaticon.com');
     })
 
     context('List of stories', () => {
-      const stories = require('../fixtures/stories.json')
+      const stories = require('../fixtures/stories.json');
 
       it('shows the right data for all rendered stories', () => {
         cy.get('.item').first().should('contain', stories.hits[0].title)
           .and('contain', stories.hits[0].author)
           .and('contain', stories.hits[0].num_comments)
-          .and('contain', stories.hits[0].points)
+          .and('contain', stories.hits[0].points);
         cy.get(`.item a:contains(${stories.hits[0].title})`)
-          .should('have.attr', 'href', stories.hits[0].url)
+          .should('have.attr', 'href', stories.hits[0].url);
 
         cy.get('.item').last().should('contain', stories.hits[1].title)
           .and('contain', stories.hits[1].author)
           .and('contain', stories.hits[1].num_comments)
-          .and('contain', stories.hits[1].points)
+          .and('contain', stories.hits[1].points);
         cy.get(`.item a:contains(${stories.hits[1].title})`)
-          .should('have.attr', 'href', stories.hits[1].url)
-      })
+          .should('have.attr', 'href', stories.hits[1].url);
+      });
 
       it('shows one less story after dimissing the first one', () => {
         cy.get('.button-small')
           .first()
-          .click()
+          .click();
 
-        cy.get('.item').should('have.length', 1)
-      })
+        cy.get('.item').should('have.length', 1);
+      });
 
       context('Order by', () => {
         it('orders by title', () => {
           cy.get('.list-header-button:contains(Title)').as('titleHeader').click();
 
-          cy.get('.item').first().should('be.visible').and('contain', stories.hits[0].title)
+          cy.get('.item').first().should('be.visible').and('contain', stories.hits[0].title);
           cy.get(`.item a:contains(${stories.hits[0].title})`)
-            .should('have.attr', 'href', stories.hits[0].url)
+            .should('have.attr', 'href', stories.hits[0].url);
 
           cy.get('@titleHeader').click();
 
-          cy.get('.item').first().should('be.visible').and('contain', stories.hits[1].title)
+          cy.get('.item').first().should('be.visible').and('contain', stories.hits[1].title);
           cy.get(`.item a:contains(${stories.hits[1].title})`)
-            .should('have.attr', 'href', stories.hits[1].url)
-        })
+            .should('have.attr', 'href', stories.hits[1].url);
+        });
           
         it('orders by author', () => {
           cy.get('.list-header-button:contains(Author)').as('authorHeader').click();
@@ -132,22 +132,22 @@ describe('Hacker Stories', () => {
 
         it('orders by comments', () => {
           cy.get('.list-header-button:contains(Comments)').as('commentsHeader').click();
-          cy.get('.item').first().should('be.visible').and('contain', stories.hits[1].num_comments)
+          cy.get('.item').first().should('be.visible').and('contain', stories.hits[1].num_comments);
           
           cy.get('@commentsHeader').click();
-          cy.get('.item').first().should('be.visible').and('contain', stories.hits[0].num_comments)
+          cy.get('.item').first().should('be.visible').and('contain', stories.hits[0].num_comments);
         })
 
         it('orders by points', () => {
           cy.get('.list-header-button:contains(Points)').as('pointsHeader').click();
-          cy.get('.item').first().should('be.visible').and('contain', stories.hits[1].points)
+          cy.get('.item').first().should('be.visible').and('contain', stories.hits[1].points);
 
           cy.get('@pointsHeader').click();
-          cy.get('.item').first().should('be.visible').and('contain', stories.hits[0].points)
+          cy.get('.item').first().should('be.visible').and('contain', stories.hits[0].points);
         });
 
-      })
-    })
+      });
+    });
 
     context('Search', () => {
       beforeEach(() => {
@@ -155,64 +155,96 @@ describe('Hacker Stories', () => {
           'GET',
           `**/search?query=${initialTerm}&page=0`,
           { fixture: 'empty' }
-        ).as('getEmptyStories')
+        ).as('getEmptyStories');
 
         cy.intercept(
           'GET',
           `**/search?query=${newTerm}&page=0`,
           { fixture: 'stories' }
-        ).as('getStories')
+        ).as('getStories');
 
-        cy.visit('/')
-        cy.wait('@getEmptyStories')
+        cy.visit('/');
+        cy.wait('@getEmptyStories');
 
         cy.get('#search')
-          .clear()
+          .clear();
       })
+
+      it('shows no story when none is returned', () => {
+        cy.get('.item').should('not.exist')
+      });
 
       it('types and hits ENTER', () => {
         cy.get('#search')
-          .type(`${newTerm}{enter}`)
+          .type(`${newTerm}{enter}`);
 
         cy.wait('@getStories')
 
-        cy.get('.item').should('have.length', 2)
+        cy.get('.item').should('have.length', 2);
         cy.get(`button:contains(${initialTerm})`)
-          .should('be.visible')
+          .should('be.visible');
       })
 
       it('types and clicks the submit button', () => {
-        cy.get('#search').type(newTerm)
-        cy.contains('Submit').click()
+        cy.get('#search').type(newTerm);
+        cy.contains('Submit').click();
 
-        cy.wait('@getStories')
+        cy.wait('@getStories');
 
-        cy.get('.item').should('have.length', 2)
+        cy.get('.item').should('have.length', 2);
         cy.get(`button:contains(${initialTerm})`)
-          .should('be.visible')
+          .should('be.visible');
       })
 
       context('Last searches', () => {
         it('shows a max of 5 buttons for the last searched terms', () => {
-          const faker = require('faker')
+          const faker = require('faker');
 
           cy.intercept(
             'GET',
             '**/search**',
             { fixture: 'empty' }
-          ).as('getRandomStories')
+          ).as('getRandomStories');
 
           Cypress._.times(6, () => {
             cy.get('#search')
               .clear()
-              .type(`${faker.random.word()}{enter}`)
-            cy.wait('@getRandomStories')
+              .type(`${faker.random.word()}{enter}`);
+            cy.wait('@getRandomStories');
           })
 
           cy.get('.last-searches button')
-            .should('have.length', 5)
+            .should('have.length', 5);
         })
       })
     })
-  })
+  });
+  
+  context.skip('Errors, simulating server and network failures', () => {
+    it('shows "Something went wrong ..." in case of a server error', () => {
+        cy.intercept(
+            'GET',
+            '**/search**',
+            { statusCode: 500 }
+        ).as('getServerFailure');
+
+        cy.visit('/');
+        cy.wait('@getServerFailure');
+
+        cy.get('p:contains(Something went wrong ...)').should('be.visible');
+    });
+
+    it('shows "Something went wrong ..." in case of a network error', () => {
+        cy.intercept(
+            'GET',
+            '**/search**',
+            { forceNetworkError: true }
+        ).as('getNetworkFailure');
+
+        cy.visit('/');
+        cy.wait('@getNetworkFailure');
+
+        cy.get('p:contains(Something went wrong ...)').should('be.visible');
+    })
+  });
 })
